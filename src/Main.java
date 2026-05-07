b74f5749740e33bf9508e558b819c33207acdc6f
Zahaawii <zahaawii@gmail.com>
2026-05-08 01:46:45 +0900

    So I did it but only with help from others.The idea I had was similar, but not entirely similar, which means that I am almost about to understand the DSA for easy problem sets which is nice. gj

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,14 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int [] test = new int []{1, 1, 2};
-        System.out.println(Arrays.toString(Easy.removeDuplicates(test)));
+        int [] nums = new int []{1, 1, 2};
+        int k = Easy.removeDuplicates(nums);
+        int[] expectedNums = {1,2};
+
+        assert k == expectedNums.length;
+        for (int i = 0; i < k; i++) {
+            assert nums[i] == expectedNums[i];
+        }
+        System.out.println(Arrays.toString(nums));
     }
     }
--- src/leetcodeEasy/Easy.java
+++ src/leetcodeEasy/Easy.java
@@ -1,5 +1,6 @@
 package leetcodeEasy;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 /*
@@ -11,17 +12,17 @@ You can return the answer in any order.
 Two sum solution:
  */
 public class Easy {
-        public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) {
 
-            HashMap<Integer, Integer> numsMap = new HashMap<>();
-            for (int i = 0; i < nums.length; i++) {
-                int temp = target - nums[i];
-                if (numsMap.containsKey(temp)) {
-                    return new int[]{numsMap.get(temp), i};
-                }
-                numsMap.put(nums[i], i);
+        HashMap<Integer, Integer> numsMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if (numsMap.containsKey(temp)) {
+                return new int[]{numsMap.get(temp), i};
             }
-            return new int[]{};
+            numsMap.put(nums[i], i);
+        }
+        return new int[]{};
        /*
        for (int i = 0; i < nums.length - 1; i++) {
                 for (int j = i + 1; j < nums.length; j++) {
@@ -33,23 +34,39 @@ public class Easy {
             return new int[]{};
         }
         */
-        }
+    }
 
-    public static int[] removeDuplicates(int[] nums) {
+    public static int removeDuplicates(int[] nums) {
 
-            // I had to check the hints and can see i've been a bit wrong about how to solve this
-            // SO now im trying my idea with using a nested for loop - still doesnt work yet
-            int length = nums.length;
+        /*
+        For this problem set we have to remove the duplicates and display the array in the correct order.
+        In order to do so, we have to initialize an index variable starting at one.
+        The idea of this is to start the loop from one, and check if there is duplicates, if there isn't then replace the current value in the array.
+        If there is, then skip that iteration until it finds a number that doesn't align.
+        The only reason this works is because we know that every duplicates is combined together
+        otherwise this wouldnt work.
 
-            int [] newNums = new int[length];
-            for (int i = 0; i < length; i++) {
-                for(int j = i + 1; j < length; j++) {
-                    newNums[i] = nums[i];
-                    if (nums[i] != nums[j]) {
-                        newNums[i] = nums[j];
-                    }
-                }
+        To display how the code works:
+        If we have 1, 1, 2 the code does the following:
+        index = 1, nums[1] = 1. nums[0 (i - 1)] = 1
+        Is the following number the same: nums[1] = 1 as nums[0 (i - 1)] = 1? Yes it is, okay then dont do anything.
+        So the array looks like [1, 1, 2]
+        index = 1, nums[2] = 2, nums[1] = 1.
+        Is the following number the same: nums[2] = 2 as nums[1] = 1? No it is not, okay then add the number add to nums[1 (index=1)] = nums[2]
+        Now the array looks like [1, 2, 2] and index becomes 2
+        index = 2, nums[3] = 2, nums[2] = 2.
+        Is the following number the same: nums[3] = 2 as nums[2 (i - 1)] = 2? Yes it is, okay then dont do anything.
+        Now the array looks like [1, 2, 2] Because we have copied the two from the next index
+         */
+        int index = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[index] = nums[i];
+                index++;
             }
-            return newNums;
+        }
+        return index;
+
     }
 }
