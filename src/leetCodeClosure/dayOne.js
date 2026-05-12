657d0dd0d8c5715aaa06b37a98dd712b80f6588a
Zahaawii <zahaawii@gmail.com>
2026-05-13 00:20:18 +0900

    Solution 2, Counter: Solved

--- src/leetCodeClosure/dayOne.js
+++ src/leetCodeClosure/dayOne.js
@@ -14,3 +14,26 @@ var createHelloWorld = function() {
  * const f = createHelloWorld();
  * f(); // "Hello World"
  */
+
+/**
+ * @param {number} n
+ * @return {Function} counter
+ * Problem set:
+ * Given an integer n, return a counter function.
+ * This counter function initially returns n and then returns 1
+ * more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+ * Solution: Simply return the counter + 1 everytime function gets called
+ */
+var createCounter = function(n) {
+
+    return function() {
+        return n++
+    };
+};
+
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
