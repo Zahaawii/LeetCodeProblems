44425545796d24698139881374178d7528c36129
Zahaawii <zahaawii@gmail.com>
2026-05-17 18:29:11 +0900

    last problem fo the day, now on to the next

--- src/leetCodeClosure/dayTwo.js
+++ src/leetCodeClosure/dayTwo.js
@@ -76,3 +76,28 @@ var filter = function(arr, fn) {
     return number;
 };
 
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ *
+ * Problem set:
+ * Given an integer array nums, a reducer function fn, and an initial value init, return the final result obtained by executing the fn function on each element of the array, sequentially, passing in the return value from the calculation on the preceding element.
+ * This result is achieved through the following operations: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, nums[2]), ... until every element in the array has been processed. The ultimate value of val is then returned.
+ * If the length of the array is 0, the function should return init.
+ * Please solve it without using the built-in Array.reduce method.
+ *
+ * Solution:
+ * Handle if the array is 0 and return the init value
+ * iterate through all the numbers in the array
+ * add the function init value +/* the array value and store it in init
+ * return init
+ */
+var reduce = function(nums, fn, init) {
+    if (nums.length < 1) return init;
+    for(let i = 0; i < nums.length; i++) {
+        init = fn(init, nums[i]);
+    }
+    return init;
+};
